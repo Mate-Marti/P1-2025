55288a8024ecd5683d6739846fa3fde0f5b09fbb
Mateo M <mateo.martinezr@uqvirtual.edu.co>
2025-11-20 15:15:22 -0500

    Delete Bankproject directory

--- Bankproject/src/main/java/proyect/bankproject/Exceptions/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package proyect.bankproject.Exceptions;
-
-public class AccountNotFoundException extends Exception{
-    public AccountNotFoundException(String message) {
-        super(message);
-    }
-}
--- Bankproject/src/main/java/proyect/bankproject/Exceptions/DuplicateUserException.java
+++
@@ -1,7 +0,0 @@
-package proyect.bankproject.Exceptions;
-
-public class DuplicateUserException extends Exception {
-    public DuplicateUserException(String message) {
-        super(message);
-    }
-}
